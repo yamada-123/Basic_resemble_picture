73d342f107ea234234fa901195bad2d694880e54
satoshi yamada <yasain0852@yahoo.co.jp>
2019-12-11 13:02:11 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users, only:[:new, :create, :show]
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
